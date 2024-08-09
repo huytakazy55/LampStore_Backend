aa1f6c1e1eb4546cfc60b16a65d5fe1861780348
Lê Quang Huy <82958067+huytakazy55@users.noreply.github.com>
2024-08-08 17:34:34 -0700

    update signup

--- Models/SignUpModel.cs
+++ Models/SignUpModel.cs
@@ -2,9 +2,6 @@ namespace LampStoreProjects.Models
 {
     public class SignUpModel
     {
-        public string? FullName { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
     }
